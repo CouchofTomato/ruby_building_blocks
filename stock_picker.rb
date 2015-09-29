562f8ccd6f32cf74bac6b2422a8d65b16b029edb
Austin P Mason <austinpmason@gmail.com>
2015-09-29 14:37:21 +0100

    Update the stock_picker to handle a negative option

--- stock_picker.rb
+++ stock_picker.rb
@@ -13,8 +13,10 @@ def stock_picker(stock_prices)
 			end
 		end
 	end
-	best_days
+	best_days.empty? ? "Sorry there were no good days to buy and sell without making a loss" : best_days
 end
 
 puts stock_picker([17,3,6,9,15,8,6,1,10]).to_s
+puts stock_picker([10,15,9,22,4,19,27,9,1]).to_s
+puts stock_picker([19,18,15,10,1])
 puts stock_picker([])
